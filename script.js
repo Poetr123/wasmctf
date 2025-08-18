5a9e1fc6d5da8f1c0df0faab5715adc0c06592b3
Poetr123 <150533614+Poetr123@users.noreply.github.com>
2025-08-18 22:00:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-(function(){
-  // flag asli
-  const flag = "ptz{js_crypt0_rev3rsal_1s_asy1k}";
+// Nice job menemukan file ini. Tapi flagnya masih tebal seperti mie instant.
+// Payload terenkripsi!
+const payload = "R1ZqZk1qVnpaV1JpWFJ3Wk1qQXhZMnhsYkdWeWJHRndlVzV6WlU9PQ==";
 
-  // step 1: convert ke base64
-  let step1 = btoa(flag);
-
-  // step 2: XOR tiap char sama kunci
-  let key = 23;
-  let step2 = step1.split("").map(c => 
-    String.fromCharCode(c.charCodeAt(0) ^ key)
-  ).join("");
-
-  // step 3: reverse string
-  let final = step2.split("").reverse().join("");
-
-  // simpan hasil di console biar attacker harus buka DevTools
-  console.log("Encrypted my pesan:", final);
-
-  // clue buat player
-  document.body.innerHTML += `<p>Pesan rahasia sudah terenkripsi. Cari di <code>console</code> kalau mau curi-curi lihat 😏</p>`;
-})();
+// Peserta: dekrip di mesinmu sendiri. No cheating from browser console ;)
